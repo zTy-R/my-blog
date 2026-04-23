63b10caf32363ac3cbf1f7b7683993cf408d748a
zTy-R <3389489974@qq.com>
2026-04-23 12:29:05 +0800

    修复首页About文案和Spoiler

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,9 +3,9 @@ import type { CardListData, Config, IntegrationUserConfig, ThemeUserConfig } fro
 export const theme: ThemeUserConfig = {
   // [Basic]
   /** Title for your website. Will be used in metadata and as browser tab title. */
-  title: 'Lukas Space-卢克斯场域',
+  title: 'Lukas场域',
   /** Will be used in index page & copyright declaration */
-  author: 'Lukas Lorentz',
+  author: 'Lukas L.',
   /** Description metadata for your website. Can be used in page metadata. */
   description: 'Stay hungry, stay foolish',
   /** The default favicon for your site which should be a path to an image in the `public/` directory. */
